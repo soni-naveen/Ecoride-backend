40b8f2657391423500e57aa6e365b1bee8166039
Naveen Soni <naveen0929.be21@chitkara.edu.in>
2024-05-13 23:21:28 +0530

    reset password code updated

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     );
     console.log("DETAILS", updatedDetails);
 
-    const url = `http://localhost:5173/update-password/${token}`;
+    const url = `https://ecoride-zzx5.onrender.com/update-password/${token}`;
 
     await mailSender(
       email,
